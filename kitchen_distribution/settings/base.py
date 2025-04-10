c165be8d662b174c52e77e59611db2c0daeb59d0
Valentin Simonov <simonov0101k@gmail.com>
2025-04-10 17:43:49 +0200

    add config

--- kitchen_distribution/settings/base.py
+++ kitchen_distribution/settings/base.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import environ
 from django.contrib import staticfiles
 from dotenv import load_dotenv
 import os
@@ -7,6 +7,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
